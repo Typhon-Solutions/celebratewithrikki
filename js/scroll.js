353be19287a2e2a82e8f11774d0b3e5c584e9374
Clive Gross <clive.gross1@gmail.com>
2017-01-07 13:37:00 +1000

    fixed scrolling js

--- js/scroll.js
+++ js/scroll.js
@@ -4,10 +4,11 @@ $(function(){
                 var speed = 1000;
                 var href= $(this).attr("href");
                 var target = $(href == "#" || href == "" ? 'html' : href);
-                var position = target.offset().top;
+                // var position = target.offset().top;
+                var position = target.get( 0 ).offsetTop;
                 $(".mdl-layout__content").animate({
                   scrollTop:position
-                }, speed, "swing");
+                }, 1500, 'easeInOutExpo');
                 console.log("SMOOTH END");
             });
         });
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,19 +1,21 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('a').bind('click', function(event) {
+        console.log("start scroll");
         var $anchor = $(this);
-        $('html, body').stop().animate({
+        $(".mdl-layout").stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        console.log("end scroll");
     });
 });
